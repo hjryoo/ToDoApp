725f41d2aeb6b162e1e47873006b27ab138011a9
HyeJin Ryoo <56506753+hjryoo@users.noreply.github.com>
2023-05-04 20:21:46 +0900

    ignore

---
+++ app/Heading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, Stylesheet } from "react-native";
+
+const Heading = () => {
+    <View style={styles.header}>
+        <Text style={styles.headerText}>
+            TodoList
+        </Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    header: {
+        marginTop: 60
+    },
+    headerText: {
+        textAlign: 'center',
+        fontSize: 80,
+        color: 'rgba(172, 23, 255, 0.33)',
+        fontWeight: 'bold'
+    }
+})
+export default Heading
+
+
