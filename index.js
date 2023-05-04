1726dcab63d08afc9719ef50b13b6cdae6e708d6
HyeJin Ryoo <56506753+hjryoo@users.noreply.github.com>
2023-05-04 23:33:28 +0900

    progress
    progress till button

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// /**
+//  * @format
+//  */
 
-AppRegistry.registerComponent(appName, () => App);
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import App from './app/App' 
+
+	const ToDoApp = () => <App />
+
+AppRegistry.registerComponent('ToDoApp', () => ToDoApp)
